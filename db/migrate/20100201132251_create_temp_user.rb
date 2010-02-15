be5bec4671cffb156af9f1277f01b00a012ff051
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2010-02-15 12:56:29 +0000

    It's more useful to be able to create this user when I want.

--- db/migrate/20100201132251_create_temp_user.rb
+++
@@ -1,9 +0,0 @@
-require 'user'
-class CreateTempUser < ActiveRecord::Migration
-  def self.up
-    User.create!(:username => 'admin', :password => 'password', :password_confirmation => 'password')
-  end
-
-  def self.down
-  end
-end

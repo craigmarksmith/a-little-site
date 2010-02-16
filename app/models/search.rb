f0f4b99711713e280e7ff563d3f7ae949b320b99
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2010-02-16 13:31:06 +0000

    Can now search by dig type

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,6 @@
 class Search
-  
-  ATTRIBUTES = [:theatre_id, :number_of_sofa_beds, :number_of_single_rooms, :number_of_twin_rooms, :number_of_double_rooms]
+
+  ATTRIBUTES = [:theatre_id, :dig_type_id, :number_of_sofa_beds, :number_of_single_rooms, :number_of_twin_rooms, :number_of_double_rooms]
   attr_accessor *ATTRIBUTES
 
   def initialize(params = {})
@@ -12,14 +12,15 @@ class Search
 
   def results
     dig_conditions = []
+    dig_conditions << "dig_types.id = :dig_type_id" if !dig_type_id.empty?
     dig_conditions << "number_of_sofa_beds >= :number_of_sofa_beds" if number_of_sofa_beds
     dig_conditions << "number_of_single_rooms >= :number_of_single_rooms" if number_of_single_rooms
     dig_conditions << "number_of_twin_rooms >= :number_of_twin_rooms" if number_of_twin_rooms
     dig_conditions << "number_of_double_rooms >= :number_of_double_rooms" if number_of_double_rooms
-    
-    Dig.find(:all, :conditions => [dig_conditions.join(' AND '),  attributes])
+
+    Dig.find(:all, :joins => [:dig_types], :conditions => [dig_conditions.join(' AND '),  attributes])
   end
-  
+
   def attributes
     ATTRIBUTES.inject({}) do |acc, attribute|
       acc.merge(attribute => self.send(attribute))

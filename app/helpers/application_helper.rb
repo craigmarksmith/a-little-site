67de9c6998f46eddf0d4d3c5cb9b6122d9729f40
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2010-04-11 10:09:29 +0100

    Show the price on the main dig page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     options.merge!({:class => 'selected'}) if (text == selected_menu_item)
     link_to text, link, options
   end
+
+  def price_per_week_from(dig)
+    number_to_currency(dig.price_per_week_from.to_i/100, {:unit => '£', :precision => 0})
+  end
 end

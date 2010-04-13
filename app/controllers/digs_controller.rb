2c625a05414682b2cd4d4f09afbc97ecc5c18fbf
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2010-04-13 16:32:20 +0100

    A little design in the digs show page.

--- app/controllers/digs_controller.rb
+++ app/controllers/digs_controller.rb
@@ -1,15 +1,15 @@
 class DigsController < ApplicationController
-  
+  layout 'one_column'
   before_filter :authorise
-  
+
   def show
     @dig = Dig.find(params[:id])
   end
-  
+
 private
   def authorise
     return if session['tour_code']
     redirect_to tour_code_entry_url(:dig_id => params[:id])
   end
-  
+
 end
